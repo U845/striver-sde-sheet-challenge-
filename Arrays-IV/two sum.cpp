54610365a3266fa5c61b6fd0bf8a6fcd83b200e1
Utkarsh Dwivedi <60837457+U845@users.noreply.github.com>
2022-06-10 00:26:30 +0530

    Check if a pair with given sum exists in Array

---
+++ Arrays-IV/two sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>um;
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(um.find(target-nums[i])!=um.end())
+            {
+                ans.push_back(i);
+                ans.push_back(um[target-nums[i]]);
+                break;
+            }
+            
+            um[nums[i]]=i;
+        }
+        return ans;
+    }
+};
