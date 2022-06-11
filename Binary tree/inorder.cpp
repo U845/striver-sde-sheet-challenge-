d6d8b7ee85b92d60b2f3b497dd09831aa430b1d2
Utkarsh Dwivedi <60837457+U845@users.noreply.github.com>
2022-06-12 00:35:45 +0530

    Inorder traversal of binary tree

---
+++ Binary tree/inorder.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*>st;
+        TreeNode* node=root;
+        vector<int>ans;
+        while(1)
+        {
+            if(node)
+            {
+                st.push(node);
+                node=node->left;
+            }
+            else{
+                if(st.empty())
+                {
+                    break;
+                }
+                node=st.top();
+                st.pop();
+                ans.push_back(node->val);
+                node=node->right;
+            }
+        }
+        return ans;
+    }
+};
