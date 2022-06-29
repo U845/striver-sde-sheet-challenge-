8d9b81c57831a0d69a8320479a2714a99e40716f
Utkarsh Dwivedi <60837457+U845@users.noreply.github.com>
2022-06-29 22:18:48 +0530

    Maximum depth of a Binary Tree

---
+++ BinaryTree-II/Maximum depth of Binary Tree.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==NULL)
+            return 0;
+        int ls=maxDepth(root->left);
+        int rs=maxDepth(root->right);
+        return max(ls,rs)+1;
+    }
+};
